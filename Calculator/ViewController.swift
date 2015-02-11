1e735b29feea93b5cb66d227f25da618cb509a04
davidgertmenianwong <dgertmenianwong@gmail.com>
2015-02-10 22:15:14 -0800

    Display button text

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var userIsTypingInitialValue = true
+
     @IBOutlet weak var display: UILabel!
 
-    
     @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        println("Digit entered: \(digit)")
+        
+        if userIsTypingInitialValue {
+            display.text = digit
+            userIsTypingInitialValue = false
+        } else {
+            display.text = display.text! + digit
+        }
     }
     
 }
